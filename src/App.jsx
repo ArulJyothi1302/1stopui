31f6046552e1bb766e9a5acfd9db9235c12be6b1
Arul <hariarul2002@gmail.com>
2025-01-30 23:54:01 +0530

    Feature update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
-import HomePage from "./components/HomePage";
 import FooterContainer from "./components/FooterContainer";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import Body from "./components/Body";
 import { Provider } from "react-redux";
-import appStore from "./utils/appstore";
+import appStore from "./utils/appStore";
 import AccountAndStatus from "./components/AccountAndStatus";
 import AccountSetting from "./components/AccountSetting";
 import Cart from "./components/Cart";
