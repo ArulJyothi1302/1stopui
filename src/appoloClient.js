94779c59d8c62a9d6c7d0f3ac497e1d59c4a0fac
Arul <hariarul2002@gmail.com>
2025-01-31 09:56:01 +0530

    awsgqluri

--- src/appoloClient.js
+++ src/appoloClient.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: import.meta.env.VITE_GRAPHQL_URI || "http://54.209.161.105/api/graphql",
+  uri: import.meta.env.VITE_GRAPHQL_URI || import.meta.env.VITE_AWS_GRAPHQL_URI,
   cache: new InMemoryCache(),
 });
 
